82055cbbce8a9b0b3b094434b60740b9148ba018
Darlan Rodrigues <darlanrodriguesp97@hotmail.com>
2020-05-07 14:32:17 -0300

    Começo da implementação do tabuleiro

---
+++ src/boardgame/Board.java
@@ -0,0 +1,20 @@
+package boardgame;
+
+
+public class Board {
+
+    private Integer rows;
+    private Integer columns;
+    private Piece[][] pieces;
+    
+    public Board(){
+        
+    }
+
+    public Board(Integer rows, Integer columns) {
+        this.rows = rows;
+        this.columns = columns;
+        pieces = new Piece[rows][columns];
+    }
+    
+}
